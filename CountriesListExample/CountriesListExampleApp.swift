97ed78d0b2c21b755c3a7b3574db00edef6d2342
Djenifer R. P <djeniferrenata@yahoo.com.br>
2022-10-09 16:12:34 -0300

    Create empty project

---
+++ CountriesListExample/CountriesListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListExampleApp.swift
+//  CountriesListExample
+//
+//  Created by Djenifer Renata Pereira on 09/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CountriesListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
