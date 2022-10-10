e79775566ffe3fa653faa1c757240615bfb7193b
Djenifer R. P <djeniferrenata@yahoo.com.br>
2022-10-10 09:49:48 -0300

    Test libraries in ContentView

--- CountriesListExample/ContentView.swift
+++ CountriesListExample/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import CountriesListLib
 
 struct ContentView: View {
+    let lib = CountriesListLib()
+    @State var continents = [String]()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("libText: \(lib.text)")
+            Divider()
+            ForEach(continents, id: \.self) { c in
+                Text(c)
+            }
         }
         .padding()
+        .onAppear {
+            lib.fetchContinents { continents in
+                self.continents = continents
+            }
+        }
     }
 }
 
--- CountriesListLib/Sources/CountriesListLib/CountriesListLib.swift
+++ CountriesListLib/Sources/CountriesListLib/CountriesListLib.swift
@@ -1,6 +1,26 @@
+import Foundation
+import Apollo
+import SchemaLib
+
 public struct CountriesListLib {
     public private(set) var text = "Hello, World!"
+    private let apolloClient: ApolloClient
 
     public init() {
+        let url = URL(string: "https://countries.trevorblades.com")!
+        apolloClient = ApolloClient(url: url)
+    }
+
+    public func fetchContinents(callback: @escaping ([String]) -> Void) {
+        let query = GetAllContinentsQuery()
+        apolloClient.fetch(query: query) { result in
+            guard let data = try? result.get().data else {
+                print("Could not fetch data")
+                return
+            }
+
+            let continents = data.continents.map { $0.name }
+            callback(continents)
+        }
     }
 }
